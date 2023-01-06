44b9919b5e8568c9c49535e1715372ef2473d660
vfurmane <vfurmane@student.42.fr>
2023-01-06 14:05:50 +0100

    feat(web): add href property to Button to make it a link

--- apps/web/components/Button.tsx
+++ apps/web/components/Button.tsx
@@ -4,10 +4,22 @@ import styles from "styles/Button.module.scss";
 export interface ButtonProps {
   children: string;
   fullWidth?: boolean;
+  href?: string;
   primary?: boolean;
 }
 
 export function Button(props: ButtonProps): ReactElement {
+  if (props.href)
+    return (
+      <a
+        className={`${styles.container} ${
+          props.fullWidth && styles.fullWidth
+        } ${props.primary && styles.primary}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
   return (
     <button
       className={`${styles.container} ${props.fullWidth && styles.fullWidth} ${
